30275b097bdd808f0536a03153bf1b82796f82fa
saveligulas <saveli.gulas@gmail.com>
2023-09-06 19:26:40 +0200

    added movement interface and added getters and setters

--- src/main/java/gulas/saveli/model/Piece.java
+++ src/main/java/gulas/saveli/model/Piece.java
@@ -1,8 +1,50 @@
 package gulas.saveli.model;
 
-public class Piece {
+public class Piece implements PieceMovement {
     private Piece_Type type;
     private char color;
     private byte  column;
     private byte row;
+
+    @Override
+    public void movePiece(byte x_coordinate, byte y_coordinate) {
+
+    }
+
+    @Override
+    public void removeFromBoard() {
+
+    }
+
+    public Piece_Type getType() {
+        return type;
+    }
+
+    public void setType(Piece_Type type) {
+        this.type = type;
+    }
+
+    public char getColor() {
+        return color;
+    }
+
+    public void setColor(char color) {
+        this.color = color;
+    }
+
+    public byte getColumn() {
+        return column;
+    }
+
+    public void setColumn(byte column) {
+        this.column = column;
+    }
+
+    public byte getRow() {
+        return row;
+    }
+
+    public void setRow(byte row) {
+        this.row = row;
+    }
 }
