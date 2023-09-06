b46187247aefb19e7842818a5d32ab6e740b7200
saveligulas <saveli.gulas@gmail.com>
2023-09-06 19:33:39 +0200

    added check

--- src/main/java/gulas/saveli/model/Piece.java
+++ src/main/java/gulas/saveli/model/Piece.java
@@ -6,16 +6,17 @@ import lombok.Data;
 public class Piece implements PieceMovement {
     private Piece_Type type;
     private char color;
-    private byte  column;
+    private byte column;
     private byte row;
 
     @Override
     public void movePiece(byte x_coordinate, byte y_coordinate) {
-
+        if(x_coordinate > 0 && x_coordinate <= MAX_COORDINATE_VALUE && y_coordinate > 0 && y_coordinate <= MAX_COORDINATE_VALUE);
     }
 
     @Override
     public void removeFromBoard() {
-
+        this.column = -1;
+        this.row = -1;
     }
 }
