185861e34c86f525ae7716813ddbf2e968ae81f6
saveligulas <saveli.gulas@gmail.com>
2023-09-07 22:29:46 +0200

    implemented interface

--- src/main/java/gulas/saveli/pieces/Pawn.java
+++ src/main/java/gulas/saveli/pieces/Pawn.java
@@ -3,8 +3,15 @@ package gulas.saveli.pieces;
 import gulas.saveli.model.Piece;
 import gulas.saveli.model.Piece_Type;
 
-public class Pawn extends Piece {
+public class Pawn extends Piece implements MovementInterface {
     public Pawn() {
         setType(Piece_Type.PAWN);
+    }
+
+
+
+    @Override
+    public boolean checkIfSquareIsAccessible(byte x_coordinate, byte y_coordinate) {
+        return false;
     }
 }
