2d3c8a3d6f6ac5e3aef0a776cd1a349354ab9f98
saveligulas <saveli.gulas@gmail.com>
2023-09-07 22:22:20 +0200

    renamed and removed method

--- src/main/java/gulas/saveli/model/PieceInterface.java
+++ src/main/java/gulas/saveli/model/PieceInterface.java
@@ -1,9 +1,8 @@
 package gulas.saveli.model;
 
-public interface PieceMovement {
+public interface PieceInterface {
     byte MAX_COORDINATE_VALUE = 7;
     char[] ALLOWED_COLORS = new char[] {'B', 'W'};
 
-    public void movePiece(byte x_coordinate, byte y_coordinate);
     public void removeFromBoard();
 }
