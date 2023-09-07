7bb21ed50440e48b564ad0b61af9b42cbdadb131
saveligulas <saveli.gulas@gmail.com>
2023-09-07 22:32:20 +0200

    added additional parameter to constructor

--- src/main/java/gulas/saveli/pieces/Pawn.java
+++ src/main/java/gulas/saveli/pieces/Pawn.java
@@ -4,14 +4,23 @@ import gulas.saveli.model.Piece;
 import gulas.saveli.model.Piece_Type;
 
 public class Pawn extends Piece implements MovementInterface {
-    public Pawn() {
+    public Pawn(boolean isWhite) {
         setType(Piece_Type.PAWN);
+        if (isWhite) {
+            setColorToWhite();
+        } else {
+            setColorToBlack();
+        }
     }
 
 
 
     @Override
     public boolean checkIfSquareIsAccessible(byte x_coordinate, byte y_coordinate) {
+        if (getColor() == 'b') {
+
+        }
+
         return false;
     }
 }
