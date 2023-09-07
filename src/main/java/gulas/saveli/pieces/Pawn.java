8201a5b0901a486d7bfdcd83e1d0dd4f39c60c9b
saveligulas <saveli.gulas@gmail.com>
2023-09-07 22:35:32 +0200

    removed interface

--- src/main/java/gulas/saveli/pieces/Pawn.java
+++ src/main/java/gulas/saveli/pieces/Pawn.java
@@ -3,7 +3,7 @@ package gulas.saveli.pieces;
 import gulas.saveli.model.Piece;
 import gulas.saveli.model.Piece_Type;
 
-public class Pawn extends Piece implements MovementInterface {
+public class Pawn extends Piece {
     public Pawn(boolean isWhite) {
         setType(Piece_Type.PAWN);
         if (isWhite) {
@@ -11,16 +11,5 @@ public class Pawn extends Piece implements MovementInterface {
         } else {
             setColorToBlack();
         }
-    }
-
-
-
-    @Override
-    public boolean checkIfSquareIsAccessible(byte x_coordinate, byte y_coordinate) {
-        if (getColor() == 'b') {
-
-        }
-
-        return false;
     }
 }
