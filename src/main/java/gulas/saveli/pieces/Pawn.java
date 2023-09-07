0656dee9a11482a405bcfad115975c466fdd9fb7
saveligulas <saveli.gulas@gmail.com>
2023-09-07 22:18:31 +0200

    added constructor

--- src/main/java/gulas/saveli/pieces/Pawn.java
+++ src/main/java/gulas/saveli/pieces/Pawn.java
@@ -1,6 +1,10 @@
 package gulas.saveli.pieces;
 
 import gulas.saveli.model.Piece;
+import gulas.saveli.model.Piece_Type;
 
 public class Pawn extends Piece {
+    public Pawn() {
+        setType(Piece_Type.PAWN);
+    }
 }
