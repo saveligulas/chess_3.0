43ec168566f078a67f1af7d70b34faa55c9a830f
saveligulas <saveli.gulas@gmail.com>
2023-09-09 20:39:15 +0200

    added column chars

--- src/main/java/gulas/saveli/model/PieceInterface.java
+++ src/main/java/gulas/saveli/model/PieceInterface.java
@@ -3,6 +3,7 @@ package gulas.saveli.model;
 public interface PieceInterface {
     byte MAX_COORDINATE_VALUE = 7;
     char[] ALLOWED_COLORS = new char[] {'B', 'W'};
+    char[] COLUMN_CHARACTER = new char[] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
 
     public void removeFromBoard();
 }
