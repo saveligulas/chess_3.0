20a7a45a15ee1ffc96584147af96d428f9fcc924
saveligulas <saveli.gulas@gmail.com>
2023-09-09 20:42:59 +0200

    added field and new method

--- src/main/java/gulas/saveli/pieces/Pawn.java
+++ src/main/java/gulas/saveli/pieces/Pawn.java
@@ -3,8 +3,12 @@ package gulas.saveli.pieces;
 import gulas.saveli.error.CoordinateOutOfBoundsException;
 import gulas.saveli.model.Piece;
 import gulas.saveli.model.Piece_Type;
+import lombok.Setter;
 
 public class Pawn extends Piece {
+    @Setter
+    private byte startingColumn;
+
     public Pawn(boolean isWhite, byte positionColumn) {
         setType(Piece_Type.PAWN);
         if (isWhite) {
@@ -16,11 +20,12 @@ public class Pawn extends Piece {
         }
 
         if (positionColumn >=0 && positionColumn <= MAX_COORDINATE_VALUE) {
-
+            setColumn(positionColumn);
+            setStartingColumn(positionColumn);
         } else {
             throw new CoordinateOutOfBoundsException();
         }
     }
 
-
+    public String getPawnNameFromStartingPosition()
 }
