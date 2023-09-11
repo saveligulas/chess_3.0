66ea2d78f70a0119bfac582c9f574b2f301dfe12
saveligulas <saveli.gulas@gmail.com>
2023-09-11 20:58:19 +0200

    added field and data annotation

--- src/main/java/gulas/saveli/Board/ChessBoard.java
+++ src/main/java/gulas/saveli/Board/ChessBoard.java
@@ -1,7 +1,12 @@
 package gulas.saveli.Board;
 
+import lombok.Data;
+
+@Data
 public class ChessBoard {
     private final Tile[] tiles;
+
+    private boolean moveOrderIsWhite = true;
     public ChessBoard() {
         this.tiles = new Tile[] {new Tile()};
     }
