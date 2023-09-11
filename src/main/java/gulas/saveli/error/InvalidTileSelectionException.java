40e2790c9d80ac44ca93039d79eca1a54a4e979f
saveligulas <saveli.gulas@gmail.com>
2023-09-11 21:21:48 +0200

    added new exception

---
+++ src/main/java/gulas/saveli/error/InvalidTileSelectionException.java
@@ -0,0 +1,5 @@
+package gulas.saveli.error;
+
+public class InvalidTileSelectionException extends RuntimeException {
+    public InvalidTileSelectionException() {super("Invalid Tile Selection");}
+}
