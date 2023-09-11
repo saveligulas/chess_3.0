03b5b451cfe089f5d45e4d2dd7973c6c203ac1ab
saveligulas <saveli.gulas@gmail.com>
2023-09-11 20:54:06 +0200

    added method

--- src/main/java/gulas/saveli/pieces/PieceMovement.java
+++ src/main/java/gulas/saveli/pieces/PieceMovement.java
@@ -4,7 +4,10 @@ import gulas.saveli.model.PieceInterface;
 
 public class PieceMovement implements PieceInterface {
 
-    public
+    public boolean pieceMovementWasSuccessful() {
+
+    }
+
 
     //TODO: implement ability to check if piece on board of specific type can move to given x and y coordinates - and add ability to check if the king is in check
 
