918068d2da7d6f63ac726d1c5f6646cde55e50fd
saveligulas <saveli.gulas@gmail.com>
2023-09-11 21:14:30 +0200

    tested new static method

--- src/main/java/gulas/saveli/MainGame.java
+++ src/main/java/gulas/saveli/MainGame.java
@@ -1,5 +1,6 @@
 package gulas.saveli;
 
+import gulas.saveli.Board.Tile;
 import gulas.saveli.pieces.Pawn;
 
 public class MainGame {
@@ -7,5 +8,13 @@ public class MainGame {
         Pawn pTest = new Pawn(false, (byte) 0);
         System.out.println(pTest.getPawnNameFromStartingPosition());
         System.out.println(pTest.toString());
+        for(Tile tile : Tile.getStandard64Tiles()) {
+            if(tile == null) {
+                System.out.println("ERROR: Tile is null");
+            } else {
+                System.out.println(tile.toString() + " | " + tile.getX_coordinate() + " | " + tile.getY_coordinate());
+            }
+
+        }
     }
 }
