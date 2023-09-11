2f1a156922aee538937ac84f15959b234148dbd1
saveligulas <saveli.gulas@gmail.com>
2023-09-11 21:04:39 +0200

    added static method

--- src/main/java/gulas/saveli/Board/Tile.java
+++ src/main/java/gulas/saveli/Board/Tile.java
@@ -6,4 +6,13 @@ import lombok.Data;
 public class Tile {
     private byte x_coordinate;
     private byte y_coordinate;
+
+    public static Tile[] getStandard64Tiles() {
+        Tile[] tiles = new Tile[64];
+        for(int rows = 0; rows < 8; rows++) {
+            for(int columns = 0; columns < 8; columns++) {
+                tiles[]
+            }
+        }
+    }
 }
