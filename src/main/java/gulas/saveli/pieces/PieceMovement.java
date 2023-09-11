a37cd9478f1bce24ae48e6fceea0d69c181c26c6
saveligulas <saveli.gulas@gmail.com>
2023-09-11 20:59:02 +0200

    fixed name of method and added parameters

--- src/main/java/gulas/saveli/pieces/PieceMovement.java
+++ src/main/java/gulas/saveli/pieces/PieceMovement.java
@@ -1,10 +1,11 @@
 package gulas.saveli.pieces;
 
+import gulas.saveli.Board.ChessBoard;
 import gulas.saveli.model.PieceInterface;
 
 public class PieceMovement implements PieceInterface {
 
-    public boolean pieceMovementWasSuccessful() {
+    public boolean pieceMovementWasSuccessful(ChessBoard board, byte x_coordinate_selection, byte y_coordinate_selection, byte x_coordinate_target, byte y_coordinate_target ) {
 
     }
 
