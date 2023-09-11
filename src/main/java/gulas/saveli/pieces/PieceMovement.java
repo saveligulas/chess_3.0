be4fcce494d080d808d16190176f97e0cce250c5
saveligulas <saveli.gulas@gmail.com>
2023-09-11 21:19:21 +0200

    added new empty methods

--- src/main/java/gulas/saveli/pieces/PieceMovement.java
+++ src/main/java/gulas/saveli/pieces/PieceMovement.java
@@ -1,14 +1,25 @@
 package gulas.saveli.pieces;
 
 import gulas.saveli.Board.ChessBoard;
+import gulas.saveli.Board.Tile;
 import gulas.saveli.model.PieceInterface;
+import gulas.saveli.model.Piece_Type;
 
 public class PieceMovement implements PieceInterface {
 
     public boolean pieceMovementWasSuccessful(ChessBoard board, byte x_coordinate_selection, byte y_coordinate_selection, byte x_coordinate_target, byte y_coordinate_target ) {
+        boolean playerIsWhite = board.isMoveOrderIsWhite();
+
 
     }
 
+    private boolean pieceOnSelectedTileHasPlayersColorAndTileIsNotEmpty(ChessBoard board, byte x_coordinate_selection, byte y_coordinate_selection) {
+        Tile[] boardTiles = board.getTiles();
+    }
+
+    private Piece_Type getPieceTypeOfSelectedTile() {
+
+    }
 
     //TODO: implement ability to check if piece on board of specific type can move to given x and y coordinates - and add ability to check if the king is in check
 
