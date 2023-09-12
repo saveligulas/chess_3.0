63e4f82eb066b7455fddf6e7bb918b133407b97b
saveligulas <saveli.gulas@gmail.com>
2023-09-12 23:52:57 +0200

    removed TODO

--- src/main/java/gulas/saveli/pieces/PieceMovement.java
+++ src/main/java/gulas/saveli/pieces/PieceMovement.java
@@ -35,8 +35,6 @@ public class PieceMovement implements PieceInterface {
         return Piece_Type.KING;
     }
 
-    //TODO: implement ability to check if piece on board of specific type can move to given x and y coordinates - and add ability to check if the king is in check
-
     @Override
     public void removeFromBoard() {
     }
