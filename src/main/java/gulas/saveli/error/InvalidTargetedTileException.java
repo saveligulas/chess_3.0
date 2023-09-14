c70cae3b476a1bf7e569575a8bad0aeb99601463
saveligulas <saveli.gulas@gmail.com>
2023-09-14 13:39:18 +0200

    added exception

---
+++ src/main/java/gulas/saveli/error/InvalidTargetedTileException.java
@@ -0,0 +1,7 @@
+package gulas.saveli.error;
+
+public class InvalidTargetedTileException extends RuntimeException {
+    public InvalidTargetedTileException() {
+        super("Invalid targeted Tile");
+    }
+}
