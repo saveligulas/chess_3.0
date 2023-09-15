b8ca2ec7cb38504fc72e1d071a4b0978438062ab
saveligulas <saveli.gulas@gmail.com>
2023-09-15 17:12:15 +0200

    implemented new name

--- src/main/java/gulas/saveli/Board/ChessBoard.java
+++ src/main/java/gulas/saveli/Board/ChessBoard.java
@@ -4,11 +4,11 @@ import lombok.Data;
 
 @Data
 public class ChessBoard {
-    private final Tile[] tiles;
+    private final LogicTile[] logicTiles;
 
     private boolean moveOrderIsWhite = true;
 
     public ChessBoard() {
-        this.tiles = Tile.getStandard64Tiles();
+        this.logicTiles = LogicTile.getStandard64Tiles();
     }
 }
