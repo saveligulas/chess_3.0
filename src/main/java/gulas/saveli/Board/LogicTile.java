d8e5bf99e3fbf274c2ee8869b08c0ee8e681e3cc
saveligulas <saveli.gulas@gmail.com>
2023-09-15 17:12:01 +0200

    refactored name

--- src/main/java/gulas/saveli/Board/LogicTile.java
+++ src/main/java/gulas/saveli/Board/LogicTile.java
@@ -4,24 +4,24 @@ import gulas.saveli.model.Piece;
 import lombok.Data;
 
 @Data
-public class Tile {
+public class LogicTile {
     private byte x_coordinate;
     private byte y_coordinate;
 
     private Piece pieceOnTile = null;
 
-    public static Tile[] getStandard64Tiles() {
-        Tile[] tiles = new Tile[64];
+    public static LogicTile[] getStandard64Tiles() {
+        LogicTile[] logicTiles = new LogicTile[64];
         for(byte rows = 0; rows < 8; rows++) {
             for(byte columns = 0; columns < 8; columns++) {
                 byte index = (byte) ((rows*8)+columns);
-                Tile tile = new Tile();
-                tile.setY_coordinate(rows);
-                tile.setX_coordinate(columns);
-                tiles[index] = tile;
+                LogicTile logicTile = new LogicTile();
+                logicTile.setY_coordinate(rows);
+                logicTile.setX_coordinate(columns);
+                logicTiles[index] = logicTile;
             }
         }
-        return tiles;
+        return logicTiles;
     }
 
     public static byte getIndex(byte x_coordinate, byte y_coordinate) {
